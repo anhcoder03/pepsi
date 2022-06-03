d5c31a2c300070d7ee9a004fd9eac2b6852b303c
A-Kenvil <nguyenphianh161003@gmail.com>
2022-06-04 06:27:34 +0700

    lan3

--- js/app.js
+++ js/app.js
@@ -1,16 +1,26 @@
-window.addEventListener("load", function() {
-    const menuToggle = this.document.querySelector('#menu-toggle');
-    const menu = this.document.querySelector('.container-header');
+// window.addEventListener("load", function() {
+//     const menuToggle = this.document.querySelector('#menu-toggle');
+//     const MenuClose = this.document.querySelector('#menu-close');
+//     const menu = this.document.querySelector('.container-header');
 
-    menuToggle && menuToggle.addEventListener("click", inMenu);
-    function inMenu(e) {
-        menu && menu.classList.add("is-show");
-    }
-    this.document.addEventListener("click", outInMenu);
-    function outInMenu(e) {
-        if (e.target.matches('#menu-toggle') || e.target.matches('.container-header')) {
-            return;
-        }
-        menu && menu.classList.remove("is-show");
-    }
-})
+//     menuToggle && menuToggle.addEventListener("click", inMenu);
+//     function inMenu(e) {
+//         menu && menu.classList.add("is-show");
+//     }
+//     // this.document.addEventListener("click", outInMenu);
+//     function outInMenu(e) {
+//         menu && menu.classList.remove("is-show");
+//     }
+// })
+
+const menuToggle = this.document.querySelector("#menu-toggle");
+const MenuClose = this.document.querySelector("#menu-close");
+const menu = this.document.querySelector(".container-header");
+const menuOut = this.document.querySelector("#menu");
+
+function inMenu() {
+    menu.classList.add('is-show');
+}
+function outInMenu() {
+    menu.classList.remove('is-show')
+}
